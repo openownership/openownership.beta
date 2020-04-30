53ad991c75ab81ab394c66c66065e2ad37cda29b
Steve Day <steve@openownership.org>
2020-04-30 15:35:42 +0100

    Replace Codekit with Gulp and Yarn
    Rather than needing to use Codekit for frontend work I wanted a free,
    CLI based way to compile the assets that was repeatable and worked
    better with git. We already had Gulp in use through Codekit, or at least
    some basic scaffolding for it, so I've  kept it for now, although we
    probably should switch to webpack for future compatibility.
    
    I've switched to Yarn to manage these packages rather than NPM/Bower
    becuase it's the defacto js package manager and it's easier to have one
    combined tool than two.
    
    The generated css/js is not totally identical because I've upgraded
    the compilers and minifiers along the way and moved a couple of vendored
    libs to node_modules, but it makes very limited visible or functional
    difference in my testing.
    
    I've removed lightgallery in this update too, because we don't seem to
    actually use it, so to clarify things it was better gone.

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+var sassPaths = [
+  'node_modules/foundation-sites/scss',
+  'node_modules/motion-ui',
+  'node_modules/aos/src/sass',
+  'node_modules/hamburgers/_sass'
+];
+
+function styles() {
+  return gulp.src('assets/scss/app.scss')
+    .pipe(
+      sass({includePaths: sassPaths})
+      .on('error', sass.logError)
+    )
+    .pipe(gulp.dest('assets/css'));
+}
+
+var jsPaths = [
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/foundation-sites/dist/js/foundation.js',
+  'node_modules/what-input/dist/what-input.js',
+  'node_modules/aos/dist/aos.js',
+  'node_modules/motion-ui/dist/motion-ui.js',
+  'node_modules/jquery.cookie/jquery.cookie.js',
+  'assets/js/app.js',
+  'assets/js/svgxuse.js'
+]
+
+function scripts() {
+  return gulp.src(jsPaths)
+    .pipe(concat('app-min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js/min'));
+}
+
+function watch() {
+  gulp.watch("assets/scss/*.scss", styles);
+  gulp.watch("assets/js/*.js", scripts);
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.build = gulp.parallel(styles, scripts)
+exports.watch = watch;
+exports.default = watch;
